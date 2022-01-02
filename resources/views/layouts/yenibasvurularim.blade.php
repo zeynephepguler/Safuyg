1e95140955913f8fd4646c20da4bd8d8012ff011
MehtapAyall <mehtapayal60@gmail.com>
2022-01-02 03:40:49 +0300

    başvuru sayfası butonları

--- resources/views/layouts/yenibasvurularim.blade.php
+++ resources/views/layouts/yenibasvurularim.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.master')
 @section('content')
-
-<input type="button" value="Çap Başvurusu Yap"> <input type="button"  value="Yatay Geçiş Başvurusu Yap"> <input type="button"  value="Dikey Geçiş Başvurusu Yap">
-<input type="button"  value="İntibak Başvurusu Yap"> <input type="button"  value="Yaz Okulu Başvurusu Yap">
+<body>
+    <link rel="stylesheet" href="{{ asset('css/sec.css') }}" type="text/css">
+<br>
+<div>
+<input type="button" class="bsvr renk" value="Çap Başvurusu">
+<input type="button" class="bsvr renk" value="Yatay Geçiş Başvurusu">
+<input type="button" class="bsvr renk" value="Dikey Geçiş Başvurusu">
+<input type="button" class="bsvr renk" value="İntibak Başvurusu">
+<input type="button" class="bsvr renk" value="Yaz Okulu Başvurusu">
+</div>
 
 @stop
